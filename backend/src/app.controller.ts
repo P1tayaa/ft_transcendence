57a72301f47403d7f97d5a71b6dadab2085ee1d2
P1tayaa <samuel.boulain@yahoo.fr>
2024-12-03 10:57:34 +0100

    backend api start

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,16 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  
+  @Get('example')
+  getExample(@Query('id') id: string): object {
+    return { message: `Received GET request`, id };
+  }
+
+  @Post('example')
+  postExample(@Body() body: any): object {
+    return { message: `Received POST request`, body };
+  }
+
+
 }
