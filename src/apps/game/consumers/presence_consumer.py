5c021b68076ec80e68424210d4f17c5d2d71dacb
omathot <116487276+omathot@users.noreply.github.com>
2025-02-24 11:28:06 +0100

    presence consummer added and routed

---
+++ src/apps/game/consumers/presence_consumer.py
@@ -0,0 +1,75 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+import json
+from django.contrib.auth.models import User
+
+class PresenceConsumer(AsyncWebsocketConsumer):
+    online_users = set()
+
+    async def connect(self):
+        user = self.scope['user']
+        if not user.is_authenticated:
+            await self.close()
+            return
+
+        PresenceConsumer.online_users.add(user.id)
+        # join presence group
+        await self.channel_layer.group_add('presence', self.channel_name)
+        await self.accept()
+
+        await self.channel_layer.group_send(
+                'presence',
+                {
+                    'type': 'user_online',
+                    'user_id': user.id,
+                    'username': user.username
+                }
+            )
+
+        # send current online users to new connected user
+        await self.send(json.dumps({
+               'type': 'online_users',
+               'users': [await self.get_user_info(user.id) for user in PresenceConsumer.online_users]
+           }))
+
+
+    async def disconnect(self, close_code):
+        user = self.scope['user']
+        if user.is_authenticated:
+            PresenceConsumer.online_users.discard(user.id)
+            # leave presence group
+            await self.channel_layer.group_discard('presence', self.channel_name)
+            await self.channel_layer.group_send(
+                'presence',
+                {
+                    'type': 'user_offline',
+                    'user_id': user.id,
+                    'username': user.username
+                }
+            )
+
+        
+    @database_sync_to_async
+    def get_user_info(self, user_id):
+        try:
+            user = User.objects.get(id=user_id)
+            return {
+                'user_id': user.id,
+                'username': user.username
+            }
+        except User.DoesNotExist:
+            return None
+
+    async def user_online(self, event):
+        await self.send(json.dumps({
+               'type': 'user_online',
+               'user_id': event['user_id'],
+               'username': event['username']
+           }))
+
+    async def user_offline(self, event):
+        await self.send(json.dumps({
+               'type': 'user_offline',
+               'user_id': event['user_id'],
+               'username': event['username']
+           }))
--- src/apps/game/routing.py
+++ src/apps/game/routing.py
@@ -1,9 +1,11 @@
 from django.urls import re_path
 from .consumers.consumers import GameConsumer, SpectatorConsumer
 from .consumers.chat_consumer import ChatConsumer
+from .consumers.status_consumer import PresenceConsumer
 from .models.matchmaking import MatchmakingConsumer
 
 websocket_urlpatterns = [
+    re_path(r'ws/presence/$', PresenceConsumer.as_asgi()),
     re_path(r'ws/room/(?P<room_name>\w+)/$', GameConsumer.as_asgi()),
     re_path(r'ws/spectate/(?P<room_name>\w+)/$', SpectatorConsumer.as_asgi()),
     re_path(r'ws/matchmaking/$', MatchmakingConsumer.as_asgi()),
