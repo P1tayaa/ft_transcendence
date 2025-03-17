0598ed0baf75bb520549f4f3c935a6b1cee60df0
omathot <116487276+omathot@users.noreply.github.com>
2025-03-17 15:28:09 +0100

    https can now serve media

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -9,5 +9,15 @@ CSRF_COOKIE_SECURE = True
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # This is a new directory
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "src",  "static", "media")
+MEDIA_URL = "/static/media/"
+
+WHITENOISE_ROOT = None  # Don't serve files from WHITENOISE_ROOT
+WHITENOISE_INDEX_FILE = False  # Don't look for index.html
+WHITENOISE_AUTOREFRESH = True  # Refresh files during development
+
+# Ensure media files are collected during collectstatic
+
