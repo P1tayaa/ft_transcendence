8b9f989df1a24f222045f2a1ff9a7b0027ed769a
Oscar Mathot <omathot@f1r3s5.codam.nl>
2025-05-05 14:31:37 +0200

    added coment for eval host computer "

--- backend/config/prod.py
+++ backend/config/prod.py
@@ -2,7 +2,8 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'nginx', 'backend']
+# EVAL: add eval computer as allowed host
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'nginx', 'backend', 'f1r3s5']
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
